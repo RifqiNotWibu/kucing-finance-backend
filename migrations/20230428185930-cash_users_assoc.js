3526efe62a2bf72de01a2960eecddcc226bf5a2e
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-04-29 02:01:06 +0700

    Added `cash` associations

---
+++ migrations/20230428185930-cash_users_assoc.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addConstraint('CASH', {
+      fields: ['userId'],
+      type: 'foreign key',
+      name: 'cash_users_assoc',
+      references: {
+         table: 'USERS',
+         field: 'id'
+      }
+     });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.addConstraint('CASH', {
+      fields: ['userId'],
+      type: 'foreign key',
+      name: 'cash_users_assoc',
+      references: {
+         table: 'USERS',
+         field: 'id'
+      }
+     });
+  }
+};
--- models/cash.js
+++ models/cash.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      CASH.belongsTo(models.USERS);
+      models.USERS.hasMany(CASH);
     }
   }
   CASH.init({
