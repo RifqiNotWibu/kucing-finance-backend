b05ee661b89598cae7a162780acbafca6b4b62bb
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-05-01 01:58:16 +0700

    Added parameter on updateUser router

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const usersControllers = require("../controllers/usersControllers.js");
 
 router.post("/signup", usersControllers.signUp);
-router.patch("/update", usersControllers.updateUser);
+router.patch("/update/:userId", usersControllers.updateUser);
 
 module.exports = router;
