08ca3d09b2f2cb094d7402c6b5b378dd4d23fd6c
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-05-01 18:32:00 +0700

    Added deleteCard controller

--- controllers/cardsControllers.js
+++ controllers/cardsControllers.js
@@ -1,5 +1,5 @@
 const { cards } = require("../models");
-
+// KURANG GET CARD
 class cardsControllers {
   static async addCard(req, res, next) {
     try {
@@ -26,6 +26,24 @@ class cardsControllers {
           where: { id },
         }
       );
+      res.status(201).json({ message: "Card updated successfully!" });
+    } catch (err) {
+      res.status(500).json({ message: `${err.message}` });
+    }
+  }
+
+  static async deleteCard(req, res) {
+    try {
+      const { id } = req.body;
+      await cards.update(
+        {
+          isActive: 0, //TAMBAH isActive DI DB
+        },
+        {
+          where: { id },
+        }
+      );
+      res.status(201).json({ message: "Card deleted successfully!" });
     } catch (err) {
       res.status(500).json({ message: `${err.message}` });
     }
