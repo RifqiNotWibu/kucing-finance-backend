d529ca7aeb7a4c7292724a0fd616e1c7c39a8511
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-05-03 10:39:56 +0700

    Added deleteCategory controller and router

--- controllers/categoriesControllers.js
+++ controllers/categoriesControllers.js
@@ -13,4 +13,23 @@ class categoriesControllers {
       res.status(500).json({ message: `${err.message}` });
     }
   }
+
+  static async deleteCategory(req, res) {
+    try {
+      const { categName } = req.body;
+      await categories.update(
+        {
+          isActive: 1,
+        },
+        {
+          where: { categName },
+        }
+      );
+      res.status(201).json({ message: "Category deleted successfully!" });
+    } catch (err) {
+      res.status(500).json({ message: `${err.message}` });
+    }
+  }
 }
+
+module.exports = categoriesControllers;
