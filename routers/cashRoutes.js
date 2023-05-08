ed189a048fedbee01c7543b0cdfa5bf78292d84f
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-05-08 14:58:28 +0700

    Added getCash route

--- routers/cashRoutes.js
+++ routers/cashRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const cashControllers = require("../controllers/cashControllers.js");
 
 router.post("/add", cashControllers.addCash);
+router.get("/get", cashControllers.getCash);
 router.patch("/update", cashControllers.updateCash);
 router.post("/delete", cashControllers.deleteCash);
 
