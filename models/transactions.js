9fd5120474aae4b28392428a1291dec258a861aa
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-05-29 20:43:27 +0700

    Added isActive column

--- models/transactions.js
+++ models/transactions.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class transactions extends Model {
     /**
@@ -13,16 +11,19 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  transactions.init({
-    id: DataTypes.INTEGER,
-    title: DataTypes.STRING,
-    type: DataTypes.STRING,
-    amount: DataTypes.INTEGER,
-    category: DataTypes.STRING,
-    description: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'transactions',
-  });
+  transactions.init(
+    {
+      title: DataTypes.STRING,
+      type: DataTypes.STRING,
+      amount: DataTypes.INTEGER,
+      category: DataTypes.STRING,
+      description: DataTypes.STRING,
+      isActive: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "transactions",
+    }
+  );
   return transactions;
 };
