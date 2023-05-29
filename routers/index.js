761bc08585766eecdd305ec1aea7ef5178791e0b
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-05-29 23:26:54 +0700

    Removed unused routers

--- routers/index.js
+++ routers/index.js
@@ -4,13 +4,7 @@ const router = express.Router();
 const userRoutes = require("./userRoutes.js");
 router.use("/user", userRoutes);
 
-const cardRoutes = require("./cardRoutes.js");
-router.use("/card", cardRoutes);
-
-const cashRoutes = require("./cashRoutes.js");
-router.use("/cash", cashRoutes);
-
-const transactionRoutes = require("./transactionRoutes.js");
-router.use("/transactions", transactionRoutes);
+const incomeRoutes = require("./incomeRoutes.js");
+router.use("", incomeRoutes);
 
 module.exports = router;
