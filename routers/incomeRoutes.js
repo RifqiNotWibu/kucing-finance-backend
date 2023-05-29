4ea3a9d908afccbf58c011db7416a5261d97272b
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-05-29 23:45:28 +0700

    Added getIncome router

--- routers/incomeRoutes.js
+++ routers/incomeRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const incomeRoutes = require("../controllers/incomeControllers.js");
 
 router.post("/addIncome", incomeRoutes.addIncome);
+router.post("/getIncome", incomeRoutes.getIncomes);
 
 module.exports = router;
