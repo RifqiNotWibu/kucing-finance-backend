657603ee7802f490a58bccea5ef88c4bceae1c37
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-05-29 15:15:18 +0700

    Added new transactions model

---
+++ models/transactions.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class transactions extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  transactions.init({
+    id: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    type: DataTypes.STRING,
+    amount: DataTypes.INTEGER,
+    category: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'transactions',
+  });
+  return transactions;
+};
