80b03b2326a9f080b077fc231739d6d27d6632f2
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-05-30 10:44:19 +0700

    Added deleteIncome router

--- routers/incomeRoutes.js
+++ routers/incomeRoutes.js
@@ -5,5 +5,6 @@ const incomeRoutes = require("../controllers/incomeControllers.js");
 
 router.post("/addIncome", incomeRoutes.addIncome);
 router.post("/getIncome", incomeRoutes.getIncomes);
+router.post("/deleteIncome/:id", incomeRoutes.deleteIncome);
 
 module.exports = router;
