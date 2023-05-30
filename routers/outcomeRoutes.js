84e652f03b04f16136c32374897f39bef9649b9a
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-05-30 10:53:55 +0700

    Added deleteOutcome router

--- routers/outcomeRoutes.js
+++ routers/outcomeRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const outcomeControllers = require("../controllers/outcomeControllers.js");
 
 router.post("/addOutcome", outcomeControllers.addOutcome);
-router.post("/getOutome", outcomeControllers.getOutcome);
+router.post("/getOutome", outcomeControllers.getOutcomes);
+router.post("/deleteOutcome/:id", outcomeControllers.deleteOutcome);
 
 module.exports = router;
