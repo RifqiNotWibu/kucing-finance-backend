166256662cd212cf29888098287007204588939f
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-06-12 22:12:22 +0700

    Added transactions-income route

--- routers/incomeRoutes.js
+++ routers/incomeRoutes.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const incomeRoutes = require("../controllers/incomeControllers.js");
+const incomeRoutes = require('../controllers/incomeControllers.js')
 
-router.post("/addIncome", incomeRoutes.addIncome);
-router.post("/getIncome", incomeRoutes.getIncomes);
-router.post("/deleteIncome/:id", incomeRoutes.deleteIncome);
+router.post('/addIncome', incomeRoutes.addIncome)
+router.get('/getIncome/:userId', incomeRoutes.getIncomes)
+router.get('/transactions-income/:userId', incomeRoutes.getIncomesByFilter)
+router.delete('/deleteIncome/:id', incomeRoutes.deleteIncome)
 
-module.exports = router;
+module.exports = router
