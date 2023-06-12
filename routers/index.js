ae46f9c3f4a1b3b9f801ae0c79b40340a08b7336
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-06-12 22:12:44 +0700

    Removed typos and unused lines

--- routers/index.js
+++ routers/index.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const userRoutes = require("./userRoutes.js");
-router.use("/user", userRoutes);
+const userRoutes = require('./userRoutes.js')
+router.use('', userRoutes)
 
-const incomeRoutes = require("./incomeRoutes.js");
-router.use("", incomeRoutes);
+const incomeRoutes = require('./incomeRoutes.js')
+router.use('', incomeRoutes)
 
-const outcomeRoutes = require("./outcomeRoutes.js");
-router.use("", outcomeRoutes);
+const expenseRoutes = require('./expenseRoutes.js')
+router.use('', expenseRoutes)
 
-module.exports = router;
+module.exports = router
--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const usersControllers = require("../controllers/usersControllers.js");
+const usersControllers = require('../controllers/usersControllers.js')
 
-router.post("/signup", usersControllers.signUp);
-router.post("/signin", usersControllers.login);
-router.patch("/update", usersControllers.updateUser);
-router.post("/delete", usersControllers.deleteUser);
-router.post("/get", usersControllers.getUser);
-router.post("/forpassemail", usersControllers.forgetPassEmail);
-router.post("/forpassotp", usersControllers.forgetPassOtp);
+router.post('/signup', usersControllers.signUp)
+router.post('/login', usersControllers.login)
+router.patch('/update', usersControllers.updateUser)
+router.post('/delete', usersControllers.deleteUser)
+router.post('/get', usersControllers.getUser)
+router.post('/forpassemail', usersControllers.forgetPassEmail)
+router.post('/forpassotp', usersControllers.forgetPassOtp)
 
-module.exports = router;
+module.exports = router
