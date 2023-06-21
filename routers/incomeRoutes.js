d4e14309dbe266bc9532aecbcdcd416ce2b5142d
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-06-21 11:01:58 +0700

    implemented verifyToken to routes

--- routers/incomeRoutes.js
+++ routers/incomeRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const verifyToken = require('../middlewares/verifyToken.js')
 
 const incomeRoutes = require('../controllers/incomeControllers.js')
 
-router.post('/addIncome', incomeRoutes.addIncome)
-router.get('/getIncome/:userId', incomeRoutes.getIncomes)
-router.get('/transactions-income/:userId', incomeRoutes.getIncomesByFilter)
-router.delete('/deleteIncome/:id', incomeRoutes.deleteIncome)
+router.post('/addIncome', verifyToken, incomeRoutes.addIncome)
+router.get('/getIncome/:userId', verifyToken, incomeRoutes.getIncomes)
+router.get(
+  '/transactions-income/:userId',
+  verifyToken,
+  incomeRoutes.getIncomesByFilter
+)
+router.delete('/deleteIncome/:id', verifyToken, incomeRoutes.deleteIncome)
 
 module.exports = router
